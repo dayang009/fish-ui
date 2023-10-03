bebc3894ecd39c771d730c532cbda0cdbb0f2b56
dayang <dayangtop@163.com>
2023-10-03 22:19:42 +0800

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,20 @@
+'use strict'
+
+/* eslint-env node */
 module.exports = {
   root: true,
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+    es6: true,
+    node: true
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
+  extends: 'eslint:recommended',
+  parserOptions: {
     "ecmaVersion": "latest",
     "sourceType": "module"
   },
+  rules: {
+    "dot-notation": 2
+  }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,19 @@
+'use strict'
+
 // vue.config.js
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   // 选项
-  lintOnSave: false
+  lintOnSave: true,
+  outputDir: 'dist',
+  assetsDir: 'static',
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 3210,
+    https: false,
+    open: true
+  },
+
 })
